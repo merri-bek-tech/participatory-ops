4f769b95420198ccd46fe8eca442d5f0404261d2
Craig Ambrose <craig@craigambrose.com>
2024-02-14 21:28:02 +1100

    added images

--- src/contexts/physical/components/RackCard.tsx
+++ src/contexts/physical/components/RackCard.tsx
@@ -1,12 +1,18 @@
-import { Card, CardHeader, Heading, Stack } from "@chakra-ui/react";
+import { Card, CardHeader, Heading, Stack, Image, Text, HStack } from "@chakra-ui/react";
 import RackPhysicalDimension from "./RackPhysicalDimension";
 import { RackSummary } from "../types";
+import Rack from "/rack.png";
 
 export default function RackCard({ name, dimensions }: RackSummary) {
   return (
     <Card>
       <CardHeader>
-        <Heading size="md">{name}</Heading>
+        <Heading size="md">
+          <HStack justify="space-between">
+            <span>{name}</span>
+            <Image src={Rack} alt="rack" height="30px" />
+          </HStack>
+        </Heading>
       </CardHeader>
 
       <Stack spacing="1px">
--- src/contexts/physical/components/SiteCard.tsx
+++ src/contexts/physical/components/SiteCard.tsx
@@ -1,13 +1,19 @@
-import { Box, Card, CardBody, CardHeader, Heading, Stack, StackDivider, useColorModeValue } from "@chakra-ui/react";
+import { Box, Card, CardBody, CardHeader, HStack, Heading, Stack, Image, useColorModeValue } from "@chakra-ui/react";
 import RackCard from "./RackCard";
 import { SiteSummary } from "../types";
+import Site from "/site.png";
 
 export default function SiteCard({ name, racks }: SiteSummary) {
   return (
     <Box>
       <Card bgColor={useColorModeValue("gray.100", "gray.900")} maxWidth="300px">
         <CardHeader>
-          <Heading size="md">{name}</Heading>
+          <Heading size="md">
+            <HStack justify="space-between">
+              <span>{name}</span>
+              <Image src={Site} alt="rack" height="30px" />
+            </HStack>
+          </Heading>
         </CardHeader>
 
         <CardBody pt={0}>
