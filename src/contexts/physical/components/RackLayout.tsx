14fb22aff68f122a47ff588402abace6e980245b
Craig Ambrose <craig@craigambrose.com>
2024-02-17 22:03:35 +1100

    rack diagram

--- src/contexts/physical/components/RackLayout.tsx
+++ src/contexts/physical/components/RackLayout.tsx
@@ -1,9 +1,12 @@
-import { Box, Stack, Image, Center } from "@chakra-ui/react"
+import { Box, Stack, Image, Center, useColorModeValue } from "@chakra-ui/react"
 import Switch1Ru from "/rack-components/switch-1ru.png"
 import Switch2Ru from "/rack-components/switch-2ru.png"
 import Server1Ru from "/rack-components/server-1ru.png"
 import Server2Ru from "/rack-components/server-2ru.png"
 import Server3Ru from "/rack-components/server-3ru.png"
+import Raid2Ru from "/rack-components/raid-2ru.png"
+import Raid3Ru from "/rack-components/raid-3ru.png"
+import Spacer1Ru from "/rack-components/spacer-1ru.png"
 
 const ruHeightOriginal = 55
 const widthOriginal = 585
@@ -11,7 +14,7 @@ const widthOriginal = 585
 const width = 400
 const ruHeight = (ruHeightOriginal / widthOriginal) * width
 
-type ComponentImageNames = "switch" | "server"
+type ComponentImageNames = "switch" | "server" | "raid" | "spacer"
 
 const images: Record<ComponentImageNames, Record<number, string>> = {
   switch: {
@@ -23,14 +26,23 @@ const images: Record<ComponentImageNames, Record<number, string>> = {
     2: Server2Ru,
     3: Server3Ru,
   },
+  raid: {
+    1: Raid2Ru,
+    2: Raid2Ru,
+    3: Raid3Ru,
+  },
+  spacer: {
+    1: Spacer1Ru,
+  },
 }
 
-function RackComponentBox({ children, ru, imageName }: { children: React.ReactNode; ru: number; imageName: ComponentImageNames }) {
-  const image = images[imageName] && images[imageName][ru]
+function RackComponentBox({ children, ru, imageName, faded }: { children?: React.ReactNode; ru: number; imageName: ComponentImageNames; faded?: boolean }) {
+  const sizes = images[imageName]
+  const image = sizes && (sizes[ru] || sizes[1])
 
   return (
-    <Box height={ruHeight * ru} pos="relative">
-      {image && <Image src={image} pos="absolute" top={0} left={0} width="100%" height="100%" opacity={0.7} zIndex={0} alt="" />}
+    <Box height={ruHeight * ru} pos="relative" opacity={faded ? 0.3 : 1.0}>
+      {image && <Image src={image} pos="absolute" top={0} left={0} width="100%" height="100%" opacity={1.0} zIndex={0} alt="" />}
 
       <Box zIndex={1} pos="absolute" top={0} left={0} width="100%" height="100%">
         {children}
@@ -67,15 +79,39 @@ function Server({ name, ru }: { name: string; ru: number }) {
   )
 }
 
+function Raid({ name, ru }: { name: string; ru: number }) {
+  return (
+    <RackComponentBox ru={ru} imageName="raid">
+      <BackgroundedStrip>{name}</BackgroundedStrip>
+    </RackComponentBox>
+  )
+}
+
+function Spacer({ ru }: { ru: number }) {
+  if (ru == 1) return <RackComponentBox ru={ru} imageName="spacer" faded={true} />
+
+  return (
+    <Box>
+      {Array.from({ length: ru }).map((_, i) => (
+        <Spacer key={i} ru={1} />
+      ))}
+    </Box>
+  )
+}
+
 export default function PhysicalLayout() {
   return (
-    <Stack maxW={width} w="100%" gap={1} backgroundColor="gray.800" padding={1}>
+    <Stack maxW={width} w="100%" gap={1} backgroundColor={useColorModeValue("gray.800", "black")} padding={1}>
       <NetworkSwitch name="Switch 1" ru={1} />
       <NetworkSwitch name="Switch 2" ru={2} />
       <Server name="Compute 1" ru={2} />
+      <Spacer ru={1} />
       <Server name="Compute 2" ru={1} />
       <Server name="Compute 2" ru={1} />
+      <Spacer ru={2} />
       <Server name="Compute 2" ru={3} />
+      <Raid name="RAID 1" ru={3} />
+      <Raid name="RAID 2" ru={2} />
     </Stack>
   )
 }
