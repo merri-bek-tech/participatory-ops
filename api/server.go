693669fd0bb73c09d02db837ff7017b4adc05e03
Craig Ambrose <craig@craigambrose.com>
2024-03-18 19:50:39 +1100

    allow api path and static paths

--- api/server.go
+++ api/server.go
@@ -2,12 +2,25 @@ package main
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	e := echo.New()
+
+	// rewrite middleware with skipper
+	e.Pre(middleware.RewriteWithConfig(middleware.RewriteConfig{
+		Skipper: func(c echo.Context) bool {
+			return strings.HasPrefix(c.Request().URL.Path, "/api")
+		},
+		Rules: map[string]string{
+			"^/*": "/",
+		},
+	}))
+
 	e.GET("/api", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
