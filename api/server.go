c440f481bbd93d7c7d05da25ea4dbf39fe94ff62
Craig Ambrose <craig@craigambrose.com>
2024-03-19 18:04:46 +1100

    API returns json inbox

--- api/server.go
+++ api/server.go
@@ -8,6 +8,11 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
+type UnknownComponent struct {
+	Uuid   string `json:"uuid"`
+	Status string `json:"status"`
+}
+
 func main() {
 	e := echo.New()
 
@@ -16,9 +21,17 @@ func main() {
 	e.GET("/api", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+	e.GET("/api/inbox", getInbox)
 
 	e.Static("/", "/app/web")
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getInbox(c echo.Context) error {
+	components := []UnknownComponent{
+		{Uuid: "f08b7172-36d8-447f-85e1-41403d2730c8", Status: "online"},
+	}
+	return c.JSON(http.StatusOK, components)
 }
 
 func rewriteExcept(paths []string, rules map[string]string) echo.MiddlewareFunc {
