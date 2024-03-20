61e717d3bdbb41276f947afa9ae78d91800a7e22
Craig Ambrose <craig@craigambrose.com>
2024-03-20 19:06:24 +1100

    sends heartbeat

--- paropd/main.go
+++ paropd/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
@@ -23,13 +25,26 @@ var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err
 }
 
 func main() {
-	var broker = "82e12caef57c4c8288d08fe23854c097.s1.eu.hivemq.cloud" // find the host name in the Overview of your cluster (see readme)
-	var port = 8883                                                    // find the port right under the host name, standard is 8883
+	client := connectClient(
+		"82e12caef57c4c8288d08fe23854c097.s1.eu.hivemq.cloud",
+		8883,
+		"paropd",
+		"be9eiQuo",
+		"test1",
+	)
+
+	// subscribe(client)
+	publishLoop(client)
+
+	client.Disconnect(250)
+}
+
+func connectClient(host string, port int, username string, password string, clientId string) mqtt.Client {
 	opts := mqtt.NewClientOptions()
-	opts.AddBroker(fmt.Sprintf("tls://%s:%d", broker, port))
-	opts.SetClientID("test1")  // set a name as you desire
-	opts.SetUsername("paropd") // these are the credentials that you declare for your cluster (see readme)
-	opts.SetPassword("be9eiQuo")
+	opts.AddBroker(fmt.Sprintf("tls://%s:%d", host, port))
+	opts.SetClientID(clientId) // set a name as you desire
+	opts.SetUsername(username) // these are the credentials that you declare for your cluster (see readme)
+	opts.SetPassword(password)
 	// (optionally) configure callback handlers that get called on certain events
 	opts.SetDefaultPublishHandler(messagePubHandler)
 	opts.OnConnect = connectHandler
@@ -41,39 +56,72 @@ func main() {
 		panic(token.Error())
 	}
 
-	// subscribe(client)
-	publish(client)
-
-	client.Disconnect(250)
+	return client
 }
 
-func subscribe(client mqtt.Client) {
-	// subscribe to the same topic, that was published to, to receive the messages
-	topic := "topic/test"
-	token := client.Subscribe(topic, 1, nil)
-	token.Wait()
-	// Check for errors during subscribe (More on error reporting https://pkg.go.dev/github.com/eclipse/paho.mqtt.golang#readme-error-handling)
-	if token.Error() != nil {
-		fmt.Print("Failed to subscribe to topic\n")
-		panic(token.Error())
-	}
-	fmt.Printf("Subscribed to topic: %s\n", topic)
-}
+// func subscribe(client mqtt.Client) {
+// 	// subscribe to the same topic, that was published to, to receive the messages
+// 	topic := "topic/test"
+// 	token := client.Subscribe(topic, 1, nil)
+// 	token.Wait()
+// 	// Check for errors during subscribe (More on error reporting https://pkg.go.dev/github.com/eclipse/paho.mqtt.golang#readme-error-handling)
+// 	if token.Error() != nil {
+// 		fmt.Print("Failed to subscribe to topic\n")
+// 		panic(token.Error())
+// 	}
+// 	fmt.Printf("Subscribed to topic: %s\n", topic)
+// }
 
-func publish(client mqtt.Client) {
-	// publish the message "Message" to the topic "topic/test" 10 times in a for loop
-	num := 10
-	for i := 0; i < num; i++ {
-		text := fmt.Sprintf("Message %d", i)
-		token := client.Publish("topic/test", 0, false, text)
-		token.Wait()
-		// Check for errors during publishing (More on error reporting https://pkg.go.dev/github.com/eclipse/paho.mqtt.golang#readme-error-handling)
-		if token.Error() != nil {
-			fmt.Printf("Failed to publish to topic")
-			panic(token.Error())
-		} else {
-			fmt.Printf("Published message %d\n", i)
-		}
+func publishLoop(client mqtt.Client) {
+	for i := 0; i < 2; i++ {
+		token := publishHeartbeat(client)
+		handleResult(token)
 		time.Sleep(time.Second)
 	}
+}
+
+func handleResult(token mqtt.Token) {
+	token.Wait()
+	// Check for errors during publishing (More on error reporting https://pkg.go.dev/github.com/eclipse/paho.mqtt.golang#readme-error-handling)
+	if token.Error() != nil {
+		fmt.Printf("Failed to publish to topic")
+		panic(token.Error())
+	} else {
+		fmt.Printf("Published message\n")
+	}
+}
+
+type Component struct {
+	Uuid   string `json:"uuid"`
+	Status string `json:"status"`
+}
+
+type Meta struct {
+	Type    string `json:"type"`
+	Version string `json:"version"`
+}
+
+func jsonString(input any) string {
+	output, err := json.Marshal(input)
+	if err != nil {
+		panic(err)
+	}
+	return string(output)
+}
+
+func publishHeartbeat(client mqtt.Client) mqtt.Token {
+	meta := Meta{
+		Type:    "heartbeat",
+		Version: "0.1.0",
+	}
+
+	payload := Component{
+		Uuid:   "f08b7172-36d8-447f-85e1-41403d2730c8",
+		Status: "online",
+	}
+
+	metaText := jsonString(meta)
+	payloadText := jsonString(payload)
+
+	return client.Publish("topic/test", 0, false, strings.Join([]string{metaText, payloadText}, "|"))
 }
