ae3a7518b6acaf9497c3a47d1b6bb645f95e3082
Craig Ambrose <craig@craigambrose.com>
2024-03-29 16:20:36 +1100

    can make config dir

--- paropd/config/config.go
+++ paropd/config/config.go
@@ -1,7 +1,8 @@
 package config
 
 import (
-	"context"
+	"fmt"
+	"os"
 	"paropd/config/computed"
 )
 
@@ -10,14 +11,80 @@ type Config struct {
 }
 
 func LoadConfig(recompute bool) (*Config, error) {
-	if recompute {
-		return nil, nil
+	configDir := ensureBestConfigDir()
+
+	fmt.Println("configDir:", configDir)
+
+	// file := configDir + "/computed.pkl"
+	// info, err := os.Stat(file)
+	// if err != nil {
+	// 	fmt.Printf("error getting file info: %v\n", err)
+	// 	return nil, err
+	// }
+	// if info.Mode().Perm()&0444 == 0444 {
+	// 	fmt.Println("file has readable permission")
+	// } else {
+	// 	fmt.Println("file does not have readable permission")
+	// }
+
+	// if recompute {
+	// 	return nil, nil
+	// }
+
+	// computedConfig, err := computed.LoadFromPath(context.Background(), configDir+"/computed.pkl")
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// return &Config{Computed: computedConfig}, nil
+	return nil, nil
+}
+
+// Private
+
+func ensureBestConfigDir() string {
+	basename := "paropd"
+	candidateDirs := []string{"/etc/" + basename, "~/.config/" + basename}
+
+	for _, dir := range candidateDirs {
+		if canReadOrMake(dir) {
+			return dir
+		}
 	}
 
-	computedConfig, err := computed.LoadFromPath(context.Background(), "./config/computed/sample.pkl")
+	panic("Could not find a suitable config directory")
+}
+
+func canReadOrMake(dir string) bool {
+	absDir := replaceTildeWithHomeDir(dir)
+	_, err := os.Stat(absDir)
+
+	if err == nil {
+		return true
+	} else if os.IsNotExist(err) {
+		return canMake(absDir)
+	} else {
+		fmt.Println("Error checking for directory:", err)
+	}
+	return false
+}
+
+func canMake(dir string) bool {
+	err := os.Mkdir(dir, 0755)
 	if err != nil {
-		return nil, err
+		fmt.Println("Error making directory:", err)
+		return false
 	}
+	return true
+}
 
-	return &Config{Computed: computedConfig}, nil
+func replaceTildeWithHomeDir(dir string) string {
+	if dir[0] == '~' {
+		homedir, err := os.UserHomeDir()
+		if err != nil {
+			panic(err)
+		}
+		return homedir + dir[1:]
+	}
+	return dir
 }
--- paropd/main.go
+++ paropd/main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"fmt"
-	comms "paropd/comms"
 	config "paropd/config"
-
-	"github.com/google/uuid"
 )
 
 func main() {
-	configData, err := config.LoadConfig(false)
-	if err != nil {
-		panic(err)
-	} else {
-		fmt.Printf("Loaded config: %+v\n", configData.Computed)
-	}
+	config.LoadConfig(false)
+	// configData, err := config.LoadConfig(false)
+	// if err != nil {
+	// 	panic(err)
+	// } else {
+	// 	fmt.Printf("Loaded config: %+v\n", configData.Computed)
+	// }
 
-	deviceId := uuid.New().String()
+	// deviceId := uuid.New().String()
 
-	client := comms.Connect(deviceId)
+	// client := comms.Connect(deviceId)
 
-	client.PublishHeartbeat()
+	// client.PublishHeartbeat()
 
-	client.Disconnect()
+	// client.Disconnect()
 }
