e504dc6d5206bf35701e3e052593a9800d24b222
Craig Ambrose <craig@craigambrose.com>
2024-03-29 19:16:59 +1100

    make it more of a looping daemon

--- paropd/main.go
+++ paropd/main.go
@@ -1,18 +1,133 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"paropd/comms"
-	config "paropd/config"
+	"io"
+	"log"
+	"os"
+	"os/signal"
+
+	"syscall"
+	"time"
+
+	comms "paropd/comms"
+	configs "paropd/config"
 )
 
+const defaultTick = 3 * time.Second
+
+// func main() {
+// 	// configData := config.LoadConfig(true)
+// 	// fmt.Printf("Loaded config: %+v\n", configData.Computed)
+
+// 	// client := comms.Connect(configData.Computed.Uuid)
+
+// 	// client.PublishHeartbeat()
+
+// 	// client.Disconnect()
+
+// 	ctx := context.Background()
+// 	ctx, cancel := context.WithCancel(ctx)
+
+// 	defer func() {
+// 		cancel()
+// 	}()
+
+// 	if err := run(ctx); err != nil {
+// 		fmt.Println("About to exit")
+// 		fmt.Fprintf(os.Stderr, "%s\n", err)
+// 		os.Exit(1)
+// 	}
+// }
+
+// type config struct {
+// 	contentType string
+// 	server      string
+// 	statusCode  int
+// 	tick        time.Duration
+// 	url         string
+// 	userAgent   string
+// }
+
+type AppData struct {
+	config *configs.Config
+	client *comms.Client
+}
+
 func main() {
-	configData := config.LoadConfig(true)
-	fmt.Printf("Loaded config: %+v\n", configData.Computed)
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
 
-	client := comms.Connect(configData.Computed.Uuid)
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGHUP)
 
-	client.PublishHeartbeat()
+	app := &AppData{}
 
-	client.Disconnect()
+	defer func() {
+		signal.Stop(signalChan)
+		app.close()
+		cancel()
+	}()
+
+	go func() {
+		for {
+			select {
+			case s := <-signalChan:
+				switch s {
+				case syscall.SIGHUP:
+					app.init()
+				case os.Interrupt:
+					fmt.Println("Interrupted.")
+					app.close()
+					cancel()
+					os.Exit(1)
+				}
+			case <-ctx.Done():
+				log.Printf("Done.")
+				os.Exit(1)
+			}
+		}
+	}()
+
+	if err := run(ctx, app, os.Stdout); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+
+func (app *AppData) init() error {
+	app.close()
+
+	log.Println("Initializing app")
+	app.config = configs.LoadConfig(true)
+	app.client = comms.Connect(app.config.Computed.Uuid)
+
+	return nil
+}
+
+func (app *AppData) close() {
+	if app.client != nil {
+		log.Println("Closing client connection")
+		app.client.Disconnect()
+		app.client = nil
+	}
+
+	if app.config != nil {
+		app.config = nil
+	}
+}
+
+func run(ctx context.Context, app *AppData, stdout io.Writer) error {
+	app.init()
+	log.SetOutput(stdout)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-time.Tick(defaultTick):
+			app.client.PublishHeartbeat()
+		}
+	}
 }
