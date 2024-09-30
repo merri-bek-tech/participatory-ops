3260b41fbe1499e773b803019d6cc40a3d681b8f
Craig Ambrose <craig@craigambrose.com>
2024-09-30 20:18:57 +1000

    inline client should be ready to use

--- api/broker/inline_client.go
+++ api/broker/inline_client.go
@@ -3,11 +3,11 @@ package broker
 import (
 	mqtt "github.com/mochi-mqtt/server/v2"
 	"github.com/mochi-mqtt/server/v2/packets"
+	msg "parops.libs/msg"
 )
 
-type genericMessageHandler func(topic string, payload []byte)
-type publishFunc func(topic string, payload []byte, retain bool, qos byte) error
-type subscribeFunc func(topic string, qos byte, handler genericMessageHandler) error
+type publishFunc func(topic string, payload string)
+type subscribeFunc func(topicFilter string, handler msg.SubscribeHandler)
 
 type InlineClient struct {
 	Publish   publishFunc
@@ -16,15 +16,15 @@ type InlineClient struct {
 
 func BuildInlineClient(server *mqtt.Server) *InlineClient {
 	return &InlineClient{
-		Publish: func(topic string, payload []byte, retain bool, qos byte) error {
-			return server.Publish(topic, payload, retain, qos)
+		Publish: func(topic string, payload string) {
+			server.Publish(topic, []byte(payload), false, 0)
 		},
-		Subscribe: func(topic string, qos byte, handler genericMessageHandler) error {
+		Subscribe: func(topicFilter string, handler msg.SubscribeHandler) {
 			callbackFn := func(cl *mqtt.Client, sub packets.Subscription, pk packets.Packet) {
 				server.Log.Info("inline client received message from subscription", "client", cl.ID, "subscriptionId", sub.Identifier, "topic", pk.TopicName, "payload", string(pk.Payload))
-				handler(pk.TopicName, pk.Payload)
+				handler(pk.TopicName, string(pk.Payload))
 			}
-			return server.Subscribe(topic, 0, callbackFn)
+			server.Subscribe(topicFilter, 1, callbackFn)
 		},
 	}
 }
